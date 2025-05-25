32eee061c97ca94627f54faa94c180f2dacec9e5
Daniel Zlatea <daniel.zlatea@onenil.eu>
2025-05-25 18:26:01 +0300

    Display image

--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,6 @@
 import { getEntries } from '../lib/contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Image from 'next/image';
 
 export async function getStaticProps() {
     const news = await getEntries('componentQuote'); // Replace with your content type ID
@@ -18,14 +19,20 @@ export default function TestPage({ news }) {
                 {news.map((item) => (
                     <li key={item.sys.id} style={{ marginBottom: '1rem' }}>
                         <h2>{item.fields.title}</h2>
-                        <p>{item.fields.date}</p>
-
-                        {/* ✅ Render the rich text field */}
+                        
                         {item.fields.quote && (
                             <div>
                                 {documentToReactComponents(item.fields.quote)}
+                                <Image
+                                    src={`https:${item.fields.image.fields.file.url}`}
+                                    alt={item.fields.title}
+                                    width={item.fields.image.fields.file.details.image.width}   // you can set this dynamically if you want
+                                    height={item.fields.image.fields.file.details.image.height}  // or extract width/height from item.fields.image.fields.file.details.image
+                                    style={{ maxWidth: '40%', height: 'auto' }}
+                                />
                             </div>
-                        )}
+
+                )}
                     </li>
                 ))}
             </ul>
