d4e96236a102b0065d9b4111bbe24da2e0b44992
Daniel Zlatea <daniel.zlatea@onenil.eu>
2025-06-01 12:05:07 +0300

    Remove pages from route for dynamic pages

--- src/app/v1/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Page(
-    props: {
-        params: Promise<{ slug: string }>
-    }
-) {
-    const params = await props.params;
-    return <h1>My Page {params.slug} </h1>
-}
